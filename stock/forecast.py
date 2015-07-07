f52e6ef204ead2ebac9cfdf6fe6794f82a4c6374
cc <testp4game@hotmail.com>
2015-07-07 19:08:06 +0800

    init project

--- stock/forecast.py
+++ stock/forecast.py
@@ -1,10 +1,9 @@
-'''
 from stock.models import Forecast, User
 class ForecastController:
     def get_list_by_user(account):
         user = User.objects.get(account=account)
         user.forecast_set.all()
-        '''
+
 
 
 
